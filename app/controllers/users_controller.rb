242dba8b71b818cdaa3fa61c7b17f55c1d0d8743
A7madXatab <Ahmadzendane725@gmail.com>
2020-10-11 18:25:31 +0300

    adding the ability to view users profiles

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by_username params[:username]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :questions, param: :title
+  resource :users
   devise_for :users
 
   post '/question/answer', to: 'questions#answer', as: 'answer_question'
+  get '/users/:username', to: 'users#show', as: 'user_show'
 
   root "questions#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
