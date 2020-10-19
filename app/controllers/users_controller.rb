f66291d4cf5c42353e053e806c7d5af09b384939
A7madXatab <Ahmadzendane725@gmail.com>
2020-10-19 22:01:56 +0300

    adding support for viewing users questions and answers using query params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find_by_username params[:username]
+    @user_items = @user.questions
+
+    @user_items = @user.answers if params[:tab] == 'answer'
   end
 end
