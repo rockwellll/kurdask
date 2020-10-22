8b5ed33a6f1b34f0924a7855fd275aba36950785
A7madXatab <Ahmadzendane725@gmail.com>
2020-10-22 20:03:21 +0300

    removing excess whitespace, adding a second query on the user in show action in case they are viewwing the user with his old username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,28 +4,30 @@ class UsersController < ApplicationController
   before_action :set_user
   before_action :is_self?, only: [:destroy, :edit]
 
-
-
   def show
-    @user = User.find_by_username params[:username]
     @user_items = @user.questions
-
     @user_items = @user.answers if params[:tab] == 'answer'
-  end
-
-  def edit
-
   end
 
   def destroy
     @user.destroy
-
     redirect_to root_path, notice: 'هه‌ژماره‌که‌ت به‌ سه‌رکه‌وتووی ڕه‌شکراوه‌'
+  end
+
+  def update
+    @user.update(username: params[:newUsername], old_username: @user.username)
+
+    unless @user.errors.messages.empty?
+      return render json: { success: false, message: @user.errors.messages[:username]}
+    end
+
+    render json: {success: true, url: user_show_url(username: @user.username)}
   end
 
   private
   def set_user
     @user = User.find_by_username params[:username]
+    @user = User.find_by_old_username params[:username] if @user.nil?
   end
 
   def is_self?
